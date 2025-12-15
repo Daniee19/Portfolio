5233efb7a15e47b4b3761ec70c374c7546a3f199
Daniee19 <F4r3ver@gmail.com>
2025-12-15 10:03:07 -0500

    feat: Se ha avanzado un nuevo portfolio con angular. Se ha avanzado con la sección del home para dispositivos móviles.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ContactComponent } from './features/contact/contact.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+
+    { path: '**', redirectTo: 'home' }
+];
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLinkActive, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  isMenuOpen = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
