a0dcb8900bf1581b0aedae79a7f0ba3381799ab0
Daniee19 <F4r3ver@gmail.com>
2025-12-17 01:15:25 -0500

    feat(projects): agregar más modularidad en project, y un poco más de personalización

---
+++ src/app/features/projects/projects.data.ts
@@ -0,0 +1,7 @@
+export const PROJECTS = {
+    allProjects: [
+        { name: 'Crypto Monitor', tecnologias: ["Java", "Spring Boot", "Spring Webflux", "Git"], description: 'Description of project 1', link: 'https://github.com/Daniee19/CryptoMonitor' },
+        { name: 'Revalisso', tecnologias: ["Angular", "Typescript", "Spring Boot", "Java", "HTML", "CSS", "Git", "Figma"], description: 'Contribute objects or resources and earn reward points, which can later be redeemed for prizes or other products.', link: 'https://github.com/Daniee19/Revalisso' },
+        { name: 'Product Barcode Manager', tecnologias: ["Java", "Git"], description: 'An inventory and sales management system developed in Java using Java Swing for the graphical user interface.', link: 'https://github.com/Daniee19/ProductBarcodeManager' }
+    ]
+}
