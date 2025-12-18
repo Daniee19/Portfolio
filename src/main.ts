23b4c6b032a09caedff24485b78863e7d1b26d46
Daniee19 <F4r3ver@gmail.com>
2025-12-18 18:43:53 -0500

    feat(projects): mejorar diseño de la sección de projects, ademas que se agregó animaciones al filtrar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+// Importar el proveedor de animaciones
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimations(), ...appConfig.providers]
+})
