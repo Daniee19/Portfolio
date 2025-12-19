34f54f858551fced2ea99b61830558ae87306716
Daniee19 <F4r3ver@gmail.com>
2025-12-19 01:43:23 -0500

    feat(contact): agregar funcionalidad para enviar mensajes por correo

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    emailJsPublicKey: 'o-gVURejl9xi_QzaX',
+    emailJsServiceId: 'service_n403sx4',
+    emailJsTemplateId: 'template_hisu6ni'
+};
