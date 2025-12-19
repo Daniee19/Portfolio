ca5898765a44f1c930db700a07e4ae12453c040b
Daniee19 <F4r3ver@gmail.com>
2025-12-19 00:15:26 -0500

    feat(contact): mejorar diseño UI/UX

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  options = [
+    "Website design", "Backend development", "Full Stack developmente", "Other"
+  ]
+
+  selectOption(option: string) {
+    //*Se le asigna un valor que antes era null
+    this.contactForm.get('topic')?.setValue(option);
+    this.contactForm.get('topic')?.markAsTouched();
+  }
+
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    message: new FormControl('', Validators.required),
+    topic: new FormControl('', Validators.required)
+  })
+
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      topic: ['', Validators.required]
+    })
+  }
+  onSubmit() {
+    alert('Form submitted!');
+  }
 
 }
