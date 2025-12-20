3d590cf4610ccc070b1a1e7e0aaacf9099bd2edd
Daniee19 <F4r3ver@gmail.com>
2025-12-20 10:55:14 -0500

    feat(other): modificar el gitignore

--- src/environments/environments.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    emailJsPublicKey: 'o-gVURejl9xi_QzaX',
-    emailJsServiceId: 'service_n403sx4',
-    emailJsTemplateId: 'template_hisu6ni'
-};
--- src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    emailJsPublicKey: 'o-gVURejl9xi_QzaX',
-    emailJsServiceId: 'service_n403sx4',
-    emailJsTemplateId: 'template_hisu6ni'
-};
